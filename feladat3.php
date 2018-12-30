0d708a2dc589045185f7c29e495a0d0d240c1c77
gyrichard <46056180+gyrichard@users.noreply.github.com>
2018-12-30 12:22:42 +0100

    feladat_3
    part two

--- feladat3.php
+++ feladat3.php
@@ -37,7 +37,59 @@ if ($part == "1") {
     $dbHaz = count($hazak, COUNT_RECURSIVE) - count($hazak);
     $_SESSION["eredmeny"] = $dbHaz;
 } elseif ($part == "2") {
+    $santaX = 0;
+    $santaY = 0;
 
+    $robotX = 0;
+    $robotY = 0;
+
+    $hazak = array();
+    $hazak[0][0] = 1;
+
+    $i = 0;
+
+    foreach ($input as $lep) {
+        if ($i % 2 == 0) {
+            switch ($lep) {
+                case '<':
+                    $santaX--;
+                    break;
+                case '^':
+                    $santaY++;
+                    break;
+                case 'v':
+                    $santaY--;
+                    break;
+                case '>':
+                    $santaX++;
+                    break;
+            }
+            if (empty($hazak[$santaX][$santaY])) {
+                $hazak[$santaX][$santaY] = 1;
+            }
+        } else {
+            switch ($lep) {
+                case '<':
+                    $robotX--;
+                    break;
+                case '^':
+                    $robotY++;
+                    break;
+                case 'v':
+                    $robotY--;
+                    break;
+                case '>':
+                    $robotX++;
+                    break;
+            }
+            if (empty($hazak[$robotX][$robotY])) {
+                $hazak[$robotX][$robotY] = 1;
+            }
+        }
+        $i++;
+    }
+    $dbHaz = count($hazak, COUNT_RECURSIVE) - count($hazak);
+    $_SESSION["eredmeny"] = $dbHaz;
 }
 
 header("location: index.php?feladat=3");
